e929242671f385091a1ef6ab7f34bfc774e3e2da
Ricardo Brunetto <ricardo@rhbrunetto.com>
2026-02-09 08:28:09 -0300

    feat: update large ble payload

--- Package.swift
+++ Package.swift
@@ -6,25 +6,27 @@ import PackageDescription
 let package = Package(
     name: "WalletStorage",
     defaultLocalization: "en",
-	platforms: [.macOS(.v13), .iOS(.v16), .watchOS(.v10)],
+    platforms: [.macOS(.v13), .iOS(.v16), .watchOS(.v10)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library( 
+        .library(
             name: "WalletStorage",
-            targets: ["WalletStorage"]),
+            targets: ["WalletStorage"])
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-log.git", from: "1.5.3"),
-		.package(url: "https://github.com/eu-digital-identity-wallet/eudi-lib-ios-iso18013-data-model.git", from: "0.7.0"),
-		],
+        .package(
+            url: "https://github.com/rhbrunetto/eudi-lib-ios-iso18013-data-model.git",
+            branch: "fix/ble-large-payload"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "WalletStorage", 
+            name: "WalletStorage",
             dependencies: [
                 .product(name: "Logging", package: "swift-log"),
-								.product(name: "MdocDataModel18013", package: "eudi-lib-ios-iso18013-data-model"),
+                .product(name: "MdocDataModel18013", package: "eudi-lib-ios-iso18013-data-model"),
             ]),
         .testTarget(
             name: "WalletStorageTests",
